ed3c6518a9526302a3e0e7cec6101bc58bf97a5c
yoonjoohye <67yjh00@gmail.com>
2020-05-29 03:21:19 +0900

    [fix] gitignore 적용

--- dist/main.834c3e99620453f1d9d1.chunk.js
+++
@@ -1,9 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["main"],{"./public/css/style.scss":
-/*!*******************************!*\
-  !*** ./public/css/style.scss ***!
-  \*******************************/
-/*! no static exports found */function(e,t,n){},"./src/index.tsx":
-/*!***********************!*\
-  !*** ./src/index.tsx ***!
-  \***********************/
-/*! no static exports found */function(e,t,n){"use strict";var r=this&&this.__createBinding||(Object.create?function(e,t,n,r){void 0===r&&(r=n),Object.defineProperty(e,r,{enumerable:!0,get:function(){return t[n]}})}:function(e,t,n,r){void 0===r&&(r=n),e[r]=t[n]}),o=this&&this.__setModuleDefault||(Object.create?function(e,t){Object.defineProperty(e,"default",{enumerable:!0,value:t})}:function(e,t){e.default=t}),i=this&&this.__importStar||function(e){if(e&&e.__esModule)return e;var t={};if(null!=e)for(var n in e)Object.hasOwnProperty.call(e,n)&&r(t,e,n);return o(t,e),t};Object.defineProperty(t,"__esModule",{value:!0});var c=i(n(/*! react */"./node_modules/react/index.js")),s=i(n(/*! react-dom */"./node_modules/react-dom/index.js"));n(/*! ../public/css/style.scss */"./public/css/style.scss");s.render(c.createElement(function(e){var t=e.color;return c.createElement("div",null,"hello world,",t)},{color:"red"}),document.getElementById("root"))}},[["./src/index.tsx","runtime","vendor"]]]);
